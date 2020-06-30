42c9b9fab2f53a1691e264d1d3da1218f8e2b654
Denis Fligič <denis.fligic@gopascz.onmicrosoft.com>
2020-06-30 17:28:45 +0200

    mapper on presentation layer

---
+++ App.Core/Data/CourseDataMap.cs
@@ -0,0 +1,23 @@
+﻿using App.Core.Models;
+using AppLibrary.Core.Models;
+using AppLibrary.Core.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App.Core.Data
+{
+    public class CourseDataMap : IModelMapper<CourseViewModel, CourseModel>
+    {
+        public IEnumerable<CourseModel> MapRangeDownwars(IEnumerable<CourseViewModel> modelRange)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<CourseViewModel> MapRangeUpwards(IEnumerable<CourseModel> modelRange)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

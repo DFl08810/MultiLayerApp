ef8c6d89ae1e09da0c42bbb1f46dfbaab5033bf6
Denis Fligič <denis.fligic@gopascz.onmicrosoft.com>
2020-07-20 13:21:17 +0200

    xx

--- App.Core/Controllers/AppManagerController.cs
+++ App.Core/Controllers/AppManagerController.cs
@@ -11,12 +11,12 @@ using Microsoft.AspNetCore.Authorization;
 namespace App.Core.Controllers
 {
     [Authorize(Roles=Role.Admin)]
-    public class AdminController : Controller
+    public class AppManagerController : Controller
     {
         private readonly UserManager<User> _userManager;
         private readonly RoleManager<IdentityRole> _roleManager;
 
-        public AdminController(UserManager<IdentityAuthLib.Models.User> userManager,
+        public AppManagerController(UserManager<IdentityAuthLib.Models.User> userManager,
                                     RoleManager<IdentityRole> roleManager)
         {
             this._userManager = userManager;
@@ -26,10 +26,12 @@ namespace App.Core.Controllers
         public async Task<IActionResult> Index()
         {
             //await CreateRolesandUsers();
-            //await CreateUsers();
             await CreateRoles();
+            await CreateUsers();
+            
             return View();
         }
+
 
         private async Task CreateRolesandUsers()
         {
@@ -40,19 +42,16 @@ namespace App.Core.Controllers
                 role.Name = Role.Admin;
                 await _roleManager.CreateAsync(role);
 
-                var user = new User();
-                user.UserName = "default";
-                user.Email = "default@default.com";
 
-                string userPWD = "somepassword";
+                //string userPWD = "somepassword";
 
-                IdentityResult chkUser = await _userManager.CreateAsync(user, userPWD);
+                //IdentityResult chkUser = await _userManager.CreateAsync(user, userPWD);
 
                 //Add default User to Role Admin    
-                if (chkUser.Succeeded)
-                {
-                    var result1 = await _userManager.AddToRoleAsync(user, Role.Admin);
-                }
+                //if (chkUser.Succeeded)
+                //{
+                //    var result1 = await _userManager.AddToRoleAsync(user, Role.Admin);
+                //}
             }
         }
 
@@ -61,12 +60,16 @@ namespace App.Core.Controllers
             var role = new IdentityRole();
             role.Name = Role.User;
             await _roleManager.CreateAsync(role);
+
+            var roleAdmin = new IdentityRole();
+            roleAdmin.Name = Role.Admin;
+            await _roleManager.CreateAsync(roleAdmin);
         }
 
         private async Task CreateUsers()
         {
             var user = new User();
-            user.UserName = "admin@admin.com";
+            user.UserName = "admin";
             user.Email = "admin@admin.com";
             user.EmailConfirmed = true;
             

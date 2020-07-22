c6ce7d2a0a3122dedf4965b1ca5858b2880604e3
Denis Fligič <denis.fligic@gopascz.onmicrosoft.com>
2020-07-22 11:31:17 +0200

    view factory comments

--- App.Core/Factories/CourseViewFactory.cs
+++ App.Core/Factories/CourseViewFactory.cs
@@ -9,18 +9,27 @@ using System.Threading.Tasks;
 
 namespace App.Core.Factories
 {
+    //Course view factory class is used to create view related objects 
     public class CourseViewFactory : ICourseViewFactory
     {
+        #region Field
         private readonly IViewModelMapper<UserViewModel, UserActionModel> _userActionMap;
         private readonly IViewModelMapper<CourseViewModel, CourseModel> _courseMapper;
+        #endregion
 
+        #region Constructor
         public CourseViewFactory(IViewModelMapper<UserViewModel, UserActionModel> userActionMap,
                                  IViewModelMapper<CourseViewModel, CourseModel> courseMapper)
         {
             this._userActionMap = userActionMap;
             this._courseMapper = courseMapper;
         }
+        #endregion
 
+        #region Method
+        //Creates list of courses with current active user, who requesed the list
+        //If user is signed on course (is present in list of users) the list is swapped with current user
+        //if user is not present, then the list is set to null
         public IEnumerable<CourseViewModel> CreateCourseForUserAction(IEnumerable<CourseModel> courseModels, UserActionModel userAction)
         {
             var outputList = new List<CourseViewModel>();
@@ -41,5 +50,7 @@ namespace App.Core.Factories
 
             return outputList;
         }
+
+        #endregion
     }
 }

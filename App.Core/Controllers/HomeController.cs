d03a75145a1428364d95da20303e37fbbbaf029a
Denis Fligič <denis.fligic@gopascz.onmicrosoft.com>
2020-07-24 11:46:48 +0200

    ffdfser

--- App.Core/Controllers/HomeController.cs
+++ App.Core/Controllers/HomeController.cs
@@ -6,20 +6,51 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using App.Core.Models;
+using Microsoft.AspNetCore.Identity;
+using IdentityAuthLib.Models;
+using App.Core.AuthInfrastructure;
 
 namespace App.Core.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly UserManager<User> _userManager;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,
+                                UserManager<IdentityAuthLib.Models.User> userManager)
         {
             _logger = logger;
+            this._userManager = userManager;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            var AuthorizedUser = await _userManager.GetUserAsync(User);
+            //initialize configuration object for view logic
+            var viewConfiguration = new ViewConfigModel();
+
+            if (AuthorizedUser == null)
+            {
+                viewConfiguration.IsSignedIn = false;
+            }
+            else
+            {
+                //if role is user, flag roles in config model
+                if (User.IsInRole(Role.User))
+                {
+                    viewConfiguration.IsUser = true;
+                    viewConfiguration.IsAdmin = false;
+                }
+                //if role is admin, reverse logic
+                if (User.IsInRole(Role.Admin))
+                {
+                    viewConfiguration.IsUser = false;
+                    viewConfiguration.IsAdmin = true;
+                }
+
+            }
+
             return View();
         }
 
---
+++ App.Core/Models/ViewConfigModel.cs
@@ -0,0 +1,18 @@
+﻿using App.Core.AuthInfrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App.Core.Models
+{
+    public class ViewConfigModel
+    {
+        //flags if is user logged in site or not
+        public bool IsSignedIn { get; set; }
+        //flags role of current user
+        public bool IsAdmin { get; set; }
+        public bool IsUser { get; set; }
+
+    }
+}
